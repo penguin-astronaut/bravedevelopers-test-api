1239fec1e106c74a01714bfc54fc66535f461dc5
Vadim Kasatkin <penguin2804@gmail.com>
2022-02-20 13:36:16 +0400

    feat: add isUserCreated in model

--- Operator.js
+++ Operator.js
@@ -4,7 +4,8 @@ const Post = new mongoose.Schema({
   name: {type: String, required: true},
   img: {type: String, required: true},
   color: {type: String, required: true},
-  slug: {type: String, required: true}
+  slug: {type: String, required: true},
+  isUserCreated: {type: Boolean, default: false}
 })
 
 export default mongoose.model('Post', Post)
